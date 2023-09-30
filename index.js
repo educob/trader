7da32c3305a725ec4038bafd5b7182167941672a
“eduardo” <educobian@gmail.com>
2023-09-30 08:52:31 +0100

    news fixed. pm2 used

--- index.js
+++ index.js
@@ -7,63 +7,114 @@ import utils from './lib/utils.js';
 import pattern from './lib/pattern.js';
 import gpt from './lib/gpt.js';
 
-
 // CODE
 const jwtToken = process.env.jwtToken
 if(!jwtToken) console.log("Must: export jwtToken=xxx")
 
 start()
 
+// pm2 start index.js --cron "*/20 * * * *" --name news -- news --exit-code 1
+// pm2 restart
+// pm2 logs news --follow
+// pm2 stop news
+// pm2 list
+// pm2 flush : reset logs
 async function start() {
+  let [ command ] = process.argv.slice(2, 3)
 
-  let db = await mongo.connect()
-  let [arg1, arg2, arg3, arg4] = process.argv.slice(2, 6)
-
-  if(!arg1) {
-    console.log("USAGE:\n bars bars_num \n gpt prompt \n jobs \n patterns")
+  if(!command) {
+    console.log("USAGE:\n bars bars_num symbolFrom \n gpt \"prompt\" \n jobs \n patterns")
     process.exit(1)
   }
 
-  if(arg1 === 'bars') {
-    const symbols = await yahoo.symbols_with_bars() // await utils.symbols_without_bars()
+  if(command === 'dayjob') {
+    let  [ force ] = process.argv.slice(3,4)
+    console.log("Starting day_job cron. force:", !!force)
+    if(!!force) {
+      await day_job()
+    }
+    const day_job_cron = new CronJob('0 0 1 * * *', day_job)
+    day_job_cron.start()    
+
+  } else if(command === 'bars') {
+    let  [barsNum, symbolFrom ] = process.argv.slice(3, 5)
+    const db = await mongo.connect()
+    //let symbols = await yahoo.symbols_with_bars() // await utils.symbols_without_bars()
+    //await db.collection('symbols_w_bars').insertOne( { symbols } )
+    let symbols = (await db.collection('symbols_w_bars').findOne( {} )).symbols
+    symbols.sort();
+    if(!!symbolFrom) {
+      let index = symbols.findIndex(symbol => symbol >= symbolFrom);
+      symbols = symbols.slice(index);
+    }
     console.log(`${symbols.length} symbols with bars`)
-    if(!arg2)
-      arg2 = 3
-    await yahoo.fetch_save_symbols(symbols, arg2)
-  } else if(arg1 === 'gpt') {  
-    if(!arg2)
-      arg2 = 'what day is today'
-    const answer = await gpt.ask(arg2)
+    if(!barsNum)
+      barsNum = 2
+    await yahoo.fetch_save_symbols(symbols, barsNum)
+  } else if(command === 'gpt') {  
+    let  [ prompt ] = process.argv.slice(3, 4)
+    if(!prompt)
+      prompt = 'what day is today'
+    const answer = await gpt.ask(prompt)
     console.log("answer:", answer)
     console.log("Thanks for using gpt")
-  } else if(arg1 === 'jobs') {
+  } else if(command === 'jobs') {
     await jobs()
     const jobs_cron = new CronJob('0 */20 * * * *', jobs)  // 1 in the morning. each minute: 0 */1 * * * *: seconds, minutes, hours day_of_month(1-31), months(0-11), day-fo-week(0-6)
     jobs_cron.start()    
-  } else if(arg1 === 'patterns') {
-    const res = await detect_patterns() ; return
-    console.log("res:", res)
-  } else if(arg1 === 'news') {
+  } else if(command === 'patterns') {
+    const res = await detect_patterns() 
+  } else if(command === 'news') {
+    const now = new Date();
+    const hours = now.getHours();
+    const minutes = now.getMinutes();
+    if ((hours === 0 && minutes >= 40) || (hours === 1) || (hours === 2) || (hours === 3)) {
+      console.log("No news processing while fetching bars and processing patterns.", new Date())
+      return
+    }
     await process_news()
-    const jobs_cron = new CronJob('0 */20 * * * *', process_news)
-    jobs_cron.start()
+    //const jobs_cron = new CronJob('0 */20 * * * *', process_news) // new CronJob('0 */20 * * * *', process_news)
+    //jobs_cron.start()
   // symbol 
-  } else if(arg1 === 'symbol') { console.log("Symbol: ", arg2)
-    if(!arg2) {
+  } else if(command === 'symbol') {    
+    let  [ symbol, subcommand, num ] = process.argv.slice(3, 6)
+     console.log("Symbol: ", subcommand)
+    if(!subcommand) {
       console.log("USAGE: symbol AAPL bars 3")
       return
     }
-    if(arg3 === 'bars') {
-      if(!arg4)
-        arg3 = 3
-      const bars = await yahoo.fetch_bars(arg2, arg4)
+    if(subcommand === 'bars') {
+      if(!num)
+        num = 3
+      const bars = await yahoo.fetch_save_bars(symbol, num)
       console.log("bars:", bars, "Total: ", bars.length)
-    } else if(arg3 === 'details') {
-      const res = await yahoo.symbol_details(arg2)
+    } else if(subcommand === 'details') {
+      const res = await yahoo.symbol_details(symbol)
       console.log("Details:", res)
     }
   }
 
+}
+
+
+async function day_job() { 
+  // check if it's sunday or monday.
+  const today = new Date();
+  const dayOfWeek = today.getDay();
+
+  if (dayOfWeek === 0 || dayOfWeek === 1) {
+    console.log('Today is either Sunday or Monday.', new Date());
+    return
+  }
+
+  // bars 2
+  const db = await mongo.connect()
+  let symbols = (await db.collection('symbols_w_bars').findOne( {} )).symbols
+  symbols.sort();
+  await yahoo.fetch_save_symbols(symbols, 2)
+  // patterns
+  await detect_patterns() 
+  console.log(`Day job run on ${(new Date()).toString().substring(0, 16)}`)
 }
 
 async function jobs() {
@@ -71,7 +122,7 @@ async function jobs() {
   process_news()
 }
 
-async function process_news() { console.warn("Start processing news")
+async function process_news() { console.info("Start processing news")
   const urls = [ 
     'https://finance.yahoo.com/screener/predefined/ms_basic_materials/',
     'https://finance.yahoo.com/screener/predefined/ms_communication_services/',
@@ -85,12 +136,21 @@ async function process_news() { console.warn("Start processing news")
     'https://finance.yahoo.com/screener/predefined/ms_technology/',
     'https://finance.yahoo.com/screener/predefined/ms_utilities/'
   ]
-  let companies_w_news = await yahoo.fetch_news(urls)
-  console.log("🖥️ 🖥️ 🖥️ 🖥️ 🖥️ companies_w_news:", companies_w_news)
-  if(!!companies_w_news.length) {
-    const res = await utils.email(`Empresas yahoo con noticias`, JSON.stringify(companies_w_news, null, 4))
-    console.log("email enviado:", res.response)
+  let { news, driver } = await yahoo.fetch_news(urls)
+  console.log("🖥️ 🖥️ 🖥️ 🖥️ 🖥️ news:", news.length, new Date())
+  const db = await mongo.connect()
+  for(const news_piece of news) {
+
+    const { symbol, url, impact, summary, reason } = news_piece
+    const details = await yahoo.symbol_details(symbol, [ 'price'])
+    const name = details.price.longName
+    console.log("symbol, url, impact, summary, reason:", symbol, url, impact, summary, reason)
+    //const res = await utils.email(`Empresas yahoo con noticias`, JSON.stringify(news, null, 4))
+    await db.collection('pattern').updateOne( { symbol }, { $set: { name, url, type: 'News', impact, summary, reason } }, { upsert: true } )
+    console.log("News recorded")
   }
+  await driver.quit()
+  //process.exit(0)
 }
 
 async function check_breakouts() {
@@ -116,21 +176,22 @@ async function check_breakouts() {
 }
 
 async function detect_patterns() {
+  const db = await mongo.connect()
+  // unlock patterns after 1 month.
+  db.collection('locked_pattern').deleteMany({ locked: { $lt: new Date() } })
   const symbols_w_bars = await utils.symbols_with_bars()
-  let count = 0
-  console.log(`checking symbols for patterns`)
-  const patterns = []
-  for(const symbol of symbols_w_bars) {
-    count++
-    if(count % 100 == 0) console.log(`${count} symbols checked for patterns`)
-    // check if already under analysis
-    const dbSymbol = await db.collection('analysis').findOne( { symbol } )
-    if(!!dbSymbol) continue
-    const res = await pattern.detect_patterns(symbol._id)
-    if(!!res.found)
-      patterns.push(res)
-  }
-  console.log("Found:", patterns.length)
+  // filters
+  const locked = await db.collection('analysis').find( { } ).toArray()
+  const analysis = await db.collection('analysis').find( { } ).toArray()
+  const symbols_w_patterns = await db.collection('pattern').find( { } ).toArray()
+  const combinedArray = symbols_w_patterns.concat(locked, analysis);
+  // remove symbols already under analysis
+  let symbols = symbols_w_bars.filter((symbol) => {
+    return !combinedArray.some((filteredSymbol) => {
+      return filteredSymbol.symbol === symbol._id;
+    });
+  });
+  await pattern.detect_patterns(symbols)
 }
 
 
